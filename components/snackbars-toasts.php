864a3c38ffdc8df12af96c03ab10caf6e8b9f5d6
zdhxiong <zdhxiong@gmail.com>
2017-02-15 22:56:44 +0800

    update

--- components/snackbars-toasts.php
+++ components/snackbars-toasts.php
@@ -9,23 +9,23 @@
 
       <div class="article-content chapter-intro">
         <div>
-          <p>Snackbars provide brief feedback about an operation through a message at the bottom of the screen. </p>
+          <p>Snackbars 在屏幕的底部提供有关操作的简洁的反馈。</p>
           <div class="col-list">
             <section class="module-module-figure col-3">
               <div class="module">
-                <p><strong>Snackbars </strong>contain a single line of text directly related to the operation performed. They may contain a text action, but no icons. </p>
-                <p><strong>Toasts </strong>(Android only) are primarily used for system messaging. They also display at the bottom of the screen, but may not be swiped off-screen. </p>
+                <p><strong>Snackbars </strong>包含和执行的操作直接相关的单行文本。它们可能包含一个文本操作，但不含图标。</p>
+                <p><strong>Toasts </strong>（仅支持 Android）主要用于系统消息。它们也显示在屏幕底部，但不能通过滑动关闭。</p>
               </div>
               <div class="module">
-                <h3><strong>Usage</strong></h3>
-                <p>Only one snackbar may be displayed at a time. Each snackbar may contain a single action, neither of which may be “Dismiss” or “Cancel.”</p>
-                <h3><strong>Behavior</strong></h3>
-                <p>Snackbars animate upwards from the bottom edge of the screen.</p>
-                <h3><strong>Snackbar specs</strong></h3>
+                <h3><strong>用法</strong></h3>
+                <p>同一时间只能有一个 snackbar 处于显示状态。每一个 snackbar 都可能包含单个操作，但不会是“Dismiss”或“Cancel”。</p>
+                <h3><strong>行为</strong></h3>
+                <p>Snackbars 从屏幕底部边缘以动画的形式出现。</p>
+                <h3><strong>Snackbar 规格</strong></h3>
                 <ul class="lst-kix_eaje492nnrn7-0 start">
-                  <li>Action button: Roboto Medium 14sp, all-caps text</li>
-                  <li>Mobile height: 48dp (single-line), 80dp (multi-line)</li>
-                  <li>Desktop snackbar height: 48dp</li>
+                  <li>操作按钮：Roboto Medium 14sp，使用大写文本</li>
+                  <li>移动端高度：48dp（单行），80dp（多行）</li>
+                  <li>桌面端高度：48dp</li>
                 </ul>
               </div>
               <div class="figure">
@@ -42,13 +42,13 @@
 
 
       <nav class="chapter-toc">
-        <h1>Contents</h1>
+        <h1>内容</h1>
         <ul>
           <li>
-            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="snackbars-toasts.html#snackbars-toasts-usage" data-qp-ui-data-id="qp:1">Usage</a>
+            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="snackbars-toasts.html#snackbars-toasts-usage" data-qp-ui-data-id="qp:1">用法</a>
           </li>
           <li>
-            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="snackbars-toasts.html#snackbars-toasts-specs" data-qp-ui-data-id="qp:2">Specs</a>
+            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="snackbars-toasts.html#snackbars-toasts-specs" data-qp-ui-data-id="qp:2">规格</a>
           </li>
         </ul>
       </nav>
@@ -56,7 +56,7 @@
       <div class="article-list">
         <div id="snackbars-toasts-usage" class="article qp-ui" data-qp-ui="{ 'ScrollSpy': {} }" data-qp-ui-data-id="qp:1">
           <h2 class="article-title qp-ui" data-qp-ui="{ 'ResponsiveZippy': {} }" data-qp-ui-data-id="qp:1">
-            Usage
+            用法
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="icon icon-expand-collapse" role="img" viewBox="0 0 10 10" aria-labelledby="title desc">
               <title>Expand and collapse content</title>
               <desc>An arrow that points down when collapsed and points up when expanded.</desc>
@@ -65,17 +65,17 @@
           </h2>
           <div class="article-content">
             <div>
-              <p>Show only one snackbar on screen at a time. </p>
-              <h1>Placement</h1>
-              <p>Snackbars appear above most elements on screen, and they are equal in elevation to the floating action button. However, they are lower in elevation than dialogs, bottom sheets, and navigation drawers.</p>
-              <h1>Behavior</h1>
-              <p>Upon entrance, snackbars animate upwards from the bottom edge of the screen. When they appear, they do not block input. </p>
-              <p>They exit by being swiped off-screen or automatically disappear after a timeout or user interaction elsewhere (such as summoning a new surface or activity). </p>
+              <p>同一时间只能有一个 snackbar 显示在屏幕上。</p>
+              <h1>位置</h1>
+              <p>Snackbars 出现在大部分元素的上方，并且它们的 Z 轴高度和浮动操作按钮相同。但是，它们的 Z 轴高度低于对话框、底部卡片和抽屉栏导航。</p>
+              <h1>行为</h1>
+              <p>snackbar 会以动画的形式从屏幕底部边缘出现。当它们出现时，它们不会阻挠用户的输入。</p>
+              <p>它们会在被滑动时消失，或者一段时间后自动消失，或者用户在 snackbar 之外的地方进行了交互时消失（例如打开新的视图或操作）。</p>
               <div class="col-list">
                 <section class="module-figure-figure col-3">
                   <div class="module">
-                    <h1>Very short text strings</h1>
-                    <p>Snackbars should contain a single line of text directly related to the operation performed. They may contain a text action. They cannot contain icons.</p>
+                    <h1>非常短的文本字符串</h1>
+                    <p>Snackbar 可以包含和执行的操作直接相关的单行文本。它们可以包含一个文本操作，但不能包含图标。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -100,9 +100,9 @@
                 </section>
                 <section class="module-figure-figure col-3">
                   <div class="module">
-                    <h1>Transient</h1>
-                    <p>Snackbars automatically time out from the screen. For usability reasons, snackbars should not contain the only way to access a core use case. They should not be persistent or be stacked, as they are above other elements on screen. </p>
-                    <p>On Android, when an unrelated dialog or popup occurs while the snackbar is up, the snackbar timeout will reset upon the window focus being regained. This is to ensure that the user will be able to read the snackbar for the full intended duration. </p>
+                    <h1>短暂显示</h1>
+                    <p>Snackbar 一段时间后会自动消失。出于可用性的原因，不应把访问核心用例的唯一途径放在 snackbar 中。因为 snackbar 会显示在其他元素之上，所以不应持续显示或者堆叠显示。</p>
+                    <p>在 Android 设备上，显示着 snackbar 时，当一个不相关的对话框或弹出控件出现时，焦点会聚焦在对话框或弹出控件上，snackbar 的超时时间会在窗口重新获得焦点时重置。这是为了确保用户能在预期时间内阅读完 snackbar 的内容。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -127,8 +127,8 @@
                 </section>
                 <section class="module-figure-figure col-3">
                   <div class="module">
-                    <h1>0-1 actions, not dismiss or cancel</h1>
-                    <p>If an action is present, comply with dialog spacing and affordance rules. For two or more actions, use a dialog, not a snackbar, even when one of the actions is a dismiss action. If the action described in the snackbar is important enough to block the use of the screen, it should be a dialog. </p>
+                    <h1>可包含 0-1 个操作，但不能是忽略或取消</h1>
+                    <p>如果存在一个操作，请遵循对话框间距和可见性规则。对于两个或更多个操作，请使用对话框，而不是 snackbar，即使其中一个是忽略操作。如果 snackbar 中描述的操作非常重要，需要屏蔽屏幕的使用，那么应该使用对话框。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -153,8 +153,8 @@
                 </section>
                 <section class="module-figure-figure col-3">
                   <div class="module">
-                    <h1>Don’t block the floating action button</h1>
-                    <p>Move your floating action button vertically to accommodate the snackbar height. </p>
+                    <h1>不要覆盖到浮动操作按钮上</h1>
+                    <p>出现 snackbar 的同时垂直移动浮动操作按钮，以便使 snackbar 不会覆盖住浮动操作按钮。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -196,8 +196,8 @@
                 </section>
                 <section class="module-figure-figure col-3">
                   <div class="module">
-                    <h1>Consecutive snackbars</h1>
-                    <p>There is only ever one snackbar displayed. When a second snackbar is triggered while the first is displayed, the first should start the contraction motion downwards before the second one animates upwards. </p>
+                    <h1>连续的 snackbar</h1>
+                    <p>始终只有一个 snackbar 处于显示状态。当第二个 snackbar 被触发，但第一个 snackbar 还显示着时，第一个 snackbar 应该开始向下隐藏，等第一个完全隐藏后，第二个 snackbar 开始向上出现。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -211,7 +211,7 @@
                         </div>
                       </div>
                       <figcaption>
-                        <p>Two consecutive snackbars with different content.</p>
+                        <p>两个有不同内容的连续的 snackbar。</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -227,7 +227,7 @@
                         </div>
                       </div>
                       <figcaption>
-                        <p>Two consecutive snackbars with the same content.</p>
+                        <p>两个有相同内容的连续的 snackbar。</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -239,7 +239,7 @@
 
         <div id="snackbars-toasts-specs" class="article qp-ui" data-qp-ui="{ 'ScrollSpy': {} }" data-qp-ui-data-id="qp:2">
           <h2 class="article-title qp-ui" data-qp-ui="{ 'ResponsiveZippy': {} }" data-qp-ui-data-id="qp:2">
-            Specs
+            规格
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="icon icon-expand-collapse" role="img" viewBox="0 0 10 10" aria-labelledby="title desc">
               <title>Expand and collapse content</title>
               <desc>An arrow that points down when collapsed and points up when expanded.</desc>
@@ -251,13 +251,13 @@
               <div class="col-list">
                 <section class="module-figure-figure col-3">
                   <div class="module">
-                    <h1>Mobile</h1>
+                    <h1>移动端</h1>
                     <ul class="lst-kix_t4bqbxm63pob-0 start">
-                      <li>Single-line snackbar height: 48dp</li>
-                      <li>Multi-line snackbar height: 80dp</li>
-                      <li>Text: Roboto Regular 14sp</li>
-                      <li>Action button: Roboto Medium 14sp, all-caps text</li>
-                      <li>Default background fill: #323232 100%</li>
+                      <li>单行 snackbar 高度：48dp</li>
+                      <li>多行 snackbar 高度：80dp</li>
+                      <li>文本：Roboto Regular 14sp</li>
+                      <li>操作按钮：Roboto Medium 14sp，全部使用大写字母</li>
+                      <li>默认背景色：#323232 100%</li>
                     </ul>
                   </div>
                   <div class="figure">
@@ -266,7 +266,7 @@
                         <img alt="" src="../static/media/components/snackbars-toasts/components_toasts_mobile1.png"/>
                       </div>
                       <figcaption>
-                        <p>A mobile snackbar</p>
+                        <p>移动端的 snackbar</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -276,7 +276,7 @@
                         <img alt="" src="../static/media/components/snackbars-toasts/components_toasts_mobile2.png"/>
                       </div>
                       <figcaption>
-                        <p>A mobile snackbar</p>
+                        <p>移动端的 snackbar</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -295,7 +295,7 @@
                         </div>
                       </div>
                       <figcaption>
-                        <p>Animation of a mobile snackbar</p>
+                        <p>移动端的 snackbar 动画</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -311,7 +311,7 @@
                         <img alt="" src="../static/media/components/snackbars-toasts/components_toasts_mobile3.png"/>
                       </div>
                       <figcaption>
-                        <p>Mobile in landscape with a snackbar</p>
+                        <p>横屏状态下的移动端 snackbar</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -324,7 +324,7 @@
                         <img alt="" src="../static/media/components/snackbars-toasts/components_toasts_mobile4.png"/>
                       </div>
                       <figcaption>
-                        <p>Specs of a mobile snackbar</p>
+                        <p>移动端的 snackbar 的规格</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -333,16 +333,16 @@
               <div class="col-list">
                 <section class="module-figure col-2">
                   <div class="module">
-                    <h1>Tablet/desktop</h1>
+                    <h1>平板/桌面端</h1>
                     <ul class="lst-kix_psrtmy7li6md-0 start">
-                      <li>Single-line snackbar height: 48dp tall</li>
-                      <li>Minimum width: 288dp</li>
-                      <li>Maximum width: 568dp</li>
-                      <li>2dp rounded corner</li>
-                      <li>Text: Roboto Regular 14sp</li>
-                      <li>Action button: Roboto Medium 14sp, all-caps text</li>
-                      <li>Default background fill: #323232 100%</li>
-                      <li>Alignment: Centered or left-aligned 24dp from the left and bottom edges of the screen</li>
+                      <li>单行 snackbar 高度：48dp</li>
+                      <li>最小宽度：288dp</li>
+                      <li>最大宽度：568dp</li>
+                      <li>2dp 圆角半径</li>
+                      <li>文本：Roboto Regular 14sp</li>
+                      <li>操作按钮：Roboto Medium 14sp，全部使用大写字母</li>
+                      <li>默认背景色：#323232 100%</li>
+                      <li>对齐方式：居中对齐，或距离屏幕左侧和底部边缘 24dp 左对齐。</li>
                     </ul>
                   </div>
                   <div class="figure">
@@ -351,7 +351,7 @@
                         <img alt="" src="../static/media/components/snackbars-toasts/components_toasts_tabletdesktop1.png"/>
                       </div>
                       <figcaption>
-                        <p>A center aligned tablet/desktop snackbar</p>
+                        <p>一个居中对齐的平板/桌面端 snackbar</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -364,7 +364,7 @@
                         <img alt="" src="../static/media/components/snackbars-toasts/components_toasts_tabletdesktop2.png"/>
                       </div>
                       <figcaption>
-                        <p>A center-aligned tablet/desktop snackbar</p>
+                        <p>一个居中对齐的平板/桌面端 snackbar</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -377,7 +377,7 @@
                         <img alt="" src="../static/media/components/snackbars-toasts/components_toasts_tabletdesktop1-left.png"/>
                       </div>
                       <figcaption>
-                        <p>A left-aligned tablet/desktop snackbar</p>
+                        <p>一个左对齐的平板/桌面端 snackbar</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -390,7 +390,7 @@
                         <img alt="" src="../static/media/components/snackbars-toasts/components_toasts_tabletdesktop2-left.png"/>
                       </div>
                       <figcaption>
-                        <p>A left-aligned tablet/desktop snackbar</p>
+                        <p>一个左对齐的平板/桌面端的 snackbar</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -403,11 +403,11 @@
                         <img alt="" src="../static/media/components/snackbars-toasts/components_toasts_tabletdesktop3.png"/>
                       </div>
                       <figcaption>
-                        <p>Specs of tablet/desktop snackbar</p>
-                        <p>Snackbar height: 48dp<br>
-                          Snackbar left and right padding: 24dp<br>
-                          Snackbar top and bottom padding: 14dp<br>
-                          Snackbar button left padding: 48dp</p>
+                        <p>平板/桌面端的 snackbar 规格</p>
+                        <p>Snackbar 高度：48dp<br>
+                          Snackbar 左右内边距：24dp<br>
+                          Snackbar 上下内边距：14dp<br>
+                          Snackbar 按钮左侧边距：48dp</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -426,7 +426,7 @@
                         </div>
                       </div>
                       <figcaption>
-                        <p>Animation of a tablet/desktop snackbar</p>
+                        <p>平板/桌面端的 snackbar 动画</p>
                       </figcaption>
                     </figure>
                   </div>
