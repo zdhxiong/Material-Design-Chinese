6e0c16c88b0792d11214f85aeb952a883365a26c
zdhxiong <zdhxiong@gmail.com>
2017-02-15 10:36:43 +0800

    update

--- components/pickers.php
+++ components/pickers.php
@@ -60,7 +60,7 @@
           </h2>
           <div class="article-content">
             <div>
-              <p>在移动设备上，选择器最适合被用来显示一个<a href="dialogs.html#dialogs-confirmation-dialogs">确认对话框</a></p>
+              <p>在移动设备上，选择器最适合被用来显示一个<a href="dialogs.html#dialogs-confirmation-dialogs">确认对话框</a>。</p>
               <p>对于内联显示，例如在一个表单中，考虑使用<a href="buttons.html#buttons-other-buttons">分段下拉按钮</a>之类的更紧凑的控件。</p>
             </div>
           </div>
@@ -80,8 +80,8 @@
               <div class="col-list">
                 <section class="module-figure col-2">
                   <div class="module">
-                    <p>A dialog picker is used to select a single date on mobile. </p>
-                    <p>The selected day is indicated by a filled circle. The current day is indicated by a different color and type weight. </p>
+                    <p>日期选择器用于在手机上选择单个日期。</p>
+                    <p>选定的日期用一个实心的圆圈表示。当前日期用一个不同颜色和类型的圆圈表示。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -89,7 +89,7 @@
                         <img alt="" src="../static/media/components/pickers/components_pickers_date1.png"/>
                       </div>
                       <figcaption>
-                        <p>Date and year picker: Light theme, portrait</p>
+                        <p>日期和年份选择器：亮色主题，竖屏</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -98,7 +98,7 @@
               <div class="col-list">
                 <section class="module-figure-module col-3">
                   <div class="module">
-                    <p>Swipe left to right to navigate through the months. Touch the year in the title bar to transition to the year view.</p>
+                    <p>从左向右滑动来选择月份。点击标题栏中的年份切换到年份视图。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -112,7 +112,7 @@
                         </div>
                       </div>
                       <figcaption>
-                        <p>Date picker navigation</p>
+                        <p>日期选择器</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -122,7 +122,7 @@
               <div class="col-list">
                 <section class="module-figure col-2">
                   <div class="module">
-                    <p>The picker view adapts to the device’s orientation. </p>
+                    <p>选择器视图会自适应设备的方向。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -130,14 +130,14 @@
                         <img alt="" src="../static/media/components/pickers/components_pickers_date3.png"/>
                       </div>
                       <figcaption>
-                        <p>Date picker: Light theme, landscape</p>
+                        <p>日期选择器：亮色主题，横屏</p>
                       </figcaption>
                     </figure>
                   </div>
                 </section>
                 <section class="module-figure col-2">
                   <div class="module">
-                    <p>A dark theme picker can complement apps that have a dark theme.</p>
+                    <p>一个暗色主题的选择器可以和暗色主题的应用更好的搭配。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -145,14 +145,14 @@
                         <img alt="" src="../static/media/components/pickers/components_pickers_date4.png"/>
                       </div>
                       <figcaption>
-                        <p>Date picker: Dark theme, portrait</p>
+                        <p>日期选择器：暗色主题，竖屏</p>
                       </figcaption>
                     </figure>
                   </div>
                 </section>
                 <section class="module-figure col-2">
                   <div class="module">
-                    <p>Landscape view of the dark theme picker.</p>
+                    <p>横屏下的暗色主题选择器。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -160,7 +160,7 @@
                         <img alt="" src="../static/media/components/pickers/components_pickers_date5.png"/>
                       </div>
                       <figcaption>
-                        <p>Date picker: Dark theme, landscape</p>
+                        <p>日期选择器：暗色主题，横屏</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -172,7 +172,7 @@
 
         <div id="pickers-time-pickers" class="article qp-ui" data-qp-ui="{ 'ScrollSpy': {} }" data-qp-ui-data-id="qp:3">
           <h2 class="article-title qp-ui" data-qp-ui="{ 'ResponsiveZippy': {} }" data-qp-ui-data-id="qp:3">
-            Time pickers
+            时间选择器
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="icon icon-expand-collapse" role="img" viewBox="0 0 10 10" aria-labelledby="title desc">
               <title>Expand and collapse content</title>
               <desc>An arrow that points down when collapsed and points up when expanded.</desc>
@@ -181,12 +181,12 @@
           </h2>
           <div class="article-content">
             <div>
-              <p>A time picker adjusts to a user’s preferred time setting, i.e. the 12-hour or 24-hour format. </p>
+              <p>时间选择器会根据用户的首选时间设置自动调整。例如，12小时或24小时格式。</p>
               <div class="col-list">
                 <section class="module-figure col-2">
                   <div class="module">
-                    <p>A dialog picker is used to select a single time (hours:minutes) on mobile. </p>
-                    <p>The selected time is indicated by the filled circle at the end of the clock hand. </p>
+                    <p>时间选择器用于在手机上选择单个时间（小时：分）。</p>
+                    <p>选中的时间由一个钟表指针末端的实心圆圈表示。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -194,7 +194,7 @@
                         <img alt="" src="../static/media/components/pickers/components_pickers_time1.png"/>
                       </div>
                       <figcaption>
-                        <p>Time picker: Light theme, portrait</p>
+                        <p>时间选择器：亮色主题，竖屏</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -203,7 +203,7 @@
               <div class="col-list">
                 <section class="module-figure-module col-3">
                   <div class="module">
-                    <p>Tap between the hour and minute label in the title bar to switch between the hour or minute view. Additionally, tap on the AM/PM label in the title bar to switch between AM or PM (in 12-hour clock mode).</p>
+                    <p>点击标题栏中的小时和分钟在小时和分钟视图之间切换。另外，点击标题栏中的 AM/PM 可以切换到 AM 或 PM（在12小时的时钟模式下）。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -217,7 +217,7 @@
                         </div>
                       </div>
                       <figcaption>
-                        <p>Time picker navigation</p>
+                        <p>时间选择器</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -227,7 +227,7 @@
               <div class="col-list">
                 <section class="module-figure col-2">
                   <div class="module">
-                    <p>The picker view adapts to the device’s orientation. </p>
+                    <p>时间选择器会自适应设备方向。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -235,14 +235,14 @@
                         <img alt="" src="../static/media/components/pickers/components_pickers_time2.png"/>
                       </div>
                       <figcaption>
-                        <p>Time picker: Light theme, landscape</p>
+                        <p>时间选择器：亮色主题，横屏</p>
                       </figcaption>
                     </figure>
                   </div>
                 </section>
                 <section class="module-figure col-2">
                   <div class="module">
-                    <p>A dark theme picker can complement apps that have a dark theme.</p>
+                    <p>暗色主题的选择器会和暗色主题应用更搭配。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -250,14 +250,14 @@
                         <img alt="" src="../static/media/components/pickers/components_pickers_time3.png"/>
                       </div>
                       <figcaption>
-                        <p>Time picker: Dark theme, portrait</p>
+                        <p>时间选择器：暗色主题，竖屏</p>
                       </figcaption>
                     </figure>
                   </div>
                 </section>
                 <section class="module-figure col-2">
                   <div class="module">
-                    <p>Landscape view of the dark theme picker </p>
+                    <p>横屏下的暗色主题选择器</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -265,7 +265,7 @@
                         <img alt="" src="../static/media/components/pickers/components_pickers_time4.png"/>
                       </div>
                       <figcaption>
-                        <p>Time picker: Dark theme, landscape</p>
+                        <p>时间选择器：暗色主题，横屏</p>
                       </figcaption>
                     </figure>
                   </div>
