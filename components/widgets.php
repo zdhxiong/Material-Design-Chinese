a63327519d7b7c740d5d7006107f2faf425d6020
zdhxiong <zdhxiong@gmail.com>
2017-02-18 10:46:06 +0800

    update

--- components/widgets.php
+++ components/widgets.php
@@ -9,21 +9,21 @@
 
       <div class="article-content chapter-intro">
         <div>
-          <p>Widgets display glanceable views of an app's most important data and functionality.</p>
+          <p>小部件显示一个供快速浏览的视图，主要用于显示应用最重要的数据和功能。</p>
         </div>
       </div>
       
       <nav class="chapter-toc">
-        <h1>Contents</h1>
+        <h1>内容</h1>
         <ul>
           <li>
-            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="widgets.html#widgets-usage" data-qp-ui-data-id="qp:1">Usage</a>
+            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="widgets.html#widgets-usage" data-qp-ui-data-id="qp:1">用法</a>
           </li>
           <li>
-            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="widgets.html#widgets-types-of-widgets" data-qp-ui-data-id="qp:2">Types of widgets</a>
+            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="widgets.html#widgets-types-of-widgets" data-qp-ui-data-id="qp:2">小部件类型</a>
           </li>
           <li>
-            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="widgets.html#widgets-behavior" data-qp-ui-data-id="qp:3">Behavior</a>
+            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="widgets.html#widgets-behavior" data-qp-ui-data-id="qp:3">行为</a>
           </li>
         </ul>
       </nav>
@@ -32,7 +32,7 @@
 
         <div id="widgets-usage" class="article qp-ui" data-qp-ui="{ 'ScrollSpy': {} }" data-qp-ui-data-id="qp:1">
           <h2 class="article-title qp-ui" data-qp-ui="{ 'ResponsiveZippy': {} }" data-qp-ui-data-id="qp:1">
-            Usage
+            用法
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="icon icon-expand-collapse" role="img" viewBox="0 0 10 10" aria-labelledby="title desc">
               <title>Expand and collapse content</title>
               <desc>An arrow that points down when collapsed and points up when expanded.</desc>
@@ -41,14 +41,14 @@
           </h2>
           <div class="article-content">
             <div>
-              <p>Home screen widgets display your app’s new and interesting content in a consolidated form. They link to richer detail within the app.</p>
-              <p>Users can move and, if supported, resize widgets across their home screen panels.</p>
+              <p>主屏幕小部件以统一的形式显示应用的新内容和有趣的内容。它们链接到应用内部的更丰富的细节。</p>
+              <p>用户可以移动小部件，如果支持的话，可以在主屏幕上调整小部件的大小。</p>
               <div class="modulettes">
                 <a class="modulette container android" href="https://www.google.com/url?q=https://developer.android.com/guide/topics/appwidgets/index.html&amp;sa=D&amp;ust=1481817989213000&amp;usg=AFQjCNEYogPWPMXxugx8F_BKM54SqwxaRA" target="_blank">
                   <img alt="" class="icon callout-icon" src="../static/images/callouts/android.svg">
                   <div class="info">
-                    <div class="title">App widgets</div>
-                    <span class="subtitle">How to publish an app widget</span>
+                    <div class="title">应用小部件</div>
+                    <span class="subtitle">如何发布一个应用小部件</span>
                   </div>
                   </img>
                 </a>
@@ -59,7 +59,7 @@
 
         <div id="widgets-types-of-widgets" class="article qp-ui" data-qp-ui="{ 'ScrollSpy': {} }" data-qp-ui-data-id="qp:2">
           <h2 class="article-title qp-ui" data-qp-ui="{ 'ResponsiveZippy': {} }" data-qp-ui-data-id="qp:2">
-            Types of widgets
+            小部件类型
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="icon icon-expand-collapse" role="img" viewBox="0 0 10 10" aria-labelledby="title desc">
               <title>Expand and collapse content</title>
               <desc>An arrow that points down when collapsed and points up when expanded.</desc>
@@ -70,8 +70,8 @@
             <div>
               <div class="col-list">
                 <section class="module-figure-module col-3">
-                  <div class="module"><h1>Information widgets</h1>
-                    <p>Information widgets display a few elements of importance to a user and track how that information changes over time, such as weather or sports scores. Tapping the widget launches the associated app into a detail view.</p>
+                  <div class="module"><h1>信息小部件</h1>
+                    <p>信息小部件向用户显示一些重要的元素，并跟踪该信息如何随着时间变化，例如天气、或运动成绩。点击小部件将启动相关的应用，并显示详细视图。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -79,7 +79,7 @@
                         <img alt="" src="../static/media/components/widgets/components_widgets_types_information.png"/>
                       </div>
                       <figcaption>
-                        <p>An information widget</p>
+                        <p>一个信息小部件</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -89,13 +89,13 @@
               <div class="col-list">
                 <section class="module-figure-figure col-3">
                   <div class="module">
-                    <h1>Collection widgets</h1>
-                    <p>Collection widgets display multiple elements of the same type, such as a collection of articles from a news app. They focus on two interactions: </p>
+                    <h1>集合小部件</h1>
+                    <p>集合小部件显示同一类型的多个元素，例如来自新闻应用的文章集合。它们专注于两种交互：</p>
                     <ul class="lst-kix_kxygxuszxhje-0 start">
-                      <li>Browsing a collection</li>
-                      <li>Opening an element’s detail view</li>
+                      <li>浏览集合</li>
+                      <li>打开一个元素的详细视图</li>
                     </ul>
-                    <p>Collection widgets can scroll vertically.</p>
+                    <p>集合小部件可以垂直滚动</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -103,7 +103,7 @@
                         <img alt="" src="../static/media/components/widgets/components_widgets_types_collection.png"/>
                       </div>
                       <figcaption>
-                        <p>Collection widgets</p>
+                        <p>集合小部件</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -119,11 +119,9 @@
               <div class="col-list">
                 <section class="module-figure-module col-3">
                   <div class="module">
-                    <h3><strong>Control widgets</strong></h3>
-                    <p>Control widgets display frequently used functions. These functions may be triggered from the home
-                      screen without opening the app. For example, music app widgets allow the user to play, pause, or
-                      skip music tracks from outside the music app.</p>
-                    <p>Control widgets may or may not progress to a detail view.</p>
+                    <h3><strong>控制小部件</strong></h3>
+                    <p>控制小部件显示常见功能。这些功能可以从主屏幕触发，而不用打开对应的应用。例如，音乐应用的小部件允许用户在音乐应用外部播放、暂停或跳过音乐曲目。</p>
+                    <p>控制小部件可能可以进入详细视图，也可能不能进入详细视图。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -131,7 +129,7 @@
                         <img alt="" src="../static/media/components/widgets/components_widgets_types_control.png"/>
                       </div>
                       <figcaption>
-                        <p>A control widget</p>
+                        <p>一个控制小部件</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -141,9 +139,9 @@
               <div class="col-list">
                 <section class="module-figure-module col-3">
                   <div class="module">
-                    <h3><strong>Hybrid widgets</strong></h3>
-                    <p>Many widgets are hybrids that combine elements of the different types above. Center your widget around one of these types and add elements of others as needed. </p>
-                    <p>For example, a music player widget combines a control widget with elements of an information widget. The result keeps the user informed about which track is currently playing. </p>
+                    <h3><strong>混合小部件</strong></h3>
+                    <p>许多小部件混合了以上多种不同类型的元素。将你的小部件居中放在这些类型的其中一个中，并根据需要添加其他元素。</p>
+                    <p>例如，音乐播放小部件把一个控制小部件和一个信息小部件混合在一起。使用户可以知道当前正在播放哪一个音轨。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -151,7 +149,7 @@
                         <img alt="" src="../static/media/components/widgets/components_widgets_types_hybrid.png"/>
                       </div>
                       <figcaption>
-                        <p>A hybrid widget</p>
+                        <p>一个混合小部件</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -164,7 +162,7 @@
 
         <div id="widgets-behavior" class="article qp-ui" data-qp-ui="{ 'ScrollSpy': {} }" data-qp-ui-data-id="qp:3">
           <h2 class="article-title qp-ui" data-qp-ui="{ 'ResponsiveZippy': {} }" data-qp-ui-data-id="qp:3">
-            Behavior
+            行为
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="icon icon-expand-collapse" role="img" viewBox="0 0 10 10" aria-labelledby="title desc">
               <title>Expand and collapse content</title>
               <desc>An arrow that points down when collapsed and points up when expanded.</desc>
@@ -173,17 +171,17 @@
           </h2>
           <div class="article-content">
             <div>
-              <h1>Navigation</h1>
-              <p>Your widgets should provide navigation links to frequently used areas of your app, including:</p>
+              <h1>导航</h1>
+              <p>你的小部件应提供指向应用常用区域的导航链接，其中包括：</p>
               <ul class="lst-kix_myvcxmhmnj8v-0 start">
-                <li>Functions that allow the user to create new content, such as a new document or message</li>
-                <li>Access to the top level of your app</li>
+                <li>允许用户创建新内容，例如新建文本或消息</li>
+                <li>允许访问应用的顶层</li>
               </ul>
               <div class="col-list">
                 <section class="module-figure-module col-3">
-                  <div class="module"><h1>Widget resizing</h1>
-                    <p>Resizing allows users to adjust the height or width of a widget. This allows users to influence the layout of widgets on home panels.</p>
-                    <p>Your app may be completely resizable or constrained to horizontal or vertical size changes.</p>
+                  <div class="module"><h1>调整小部件大小</h1>
+                    <p>用户可以调整小部件的高度和宽度。这可以使用户调整主屏幕上的小部件的布局。</p>
+                    <p>你的应用可以同时支持横向纵向调整大小，或只支持横向、或只支持纵向调整大小。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -191,7 +189,7 @@
                         <img alt="" src="../static/media/components/widgets/components_widgets_behavior_01.png"/>
                       </div>
                       <figcaption>
-                        <p>A long press and subsequent release sets resizable widgets into resize mode. Users can use the drag handles or the widget corners to set the desired size.</p>
+                        <p>长按之后可调整大小的小部件将进入调整大小模式。用户可以拖动手柄或小部件角度来设置所需的大小。</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -201,33 +199,32 @@
               <div class="col-list">
                 <section class="module col-1">
                   <div class="module">
-                    <h2>Scrollable widgets</h2>
-                    <p>List or grid-based collection widgets usually expand or contract the vertical scrolling area. Regardless of the widget's size, the user can still scroll all elements into view.</p>
-                    <p>Determine how much of your app's information should surface. For smaller sizes concentrate on the essential and then add more contextual information as the widget grows.</p>
+                    <h2>可滚动小部件</h2>
+                    <p>列表或基于网格的集合小部件通常会展开或收缩垂直滚动区域。无论小部件大小，用户都可以滚动视图内的所有元素。</p>
+                    <p>确定应该显示多少应用信息。对于较小的小部件，只显示信息要点，并在展开小部件时添加更多相关信息。</p>
                   </div>
                 </section>
                 <section class="module col-1">
                   <div class="module">
-                    <h2>Non-scrollable widgets</h2>
-                    <p>Information widgets are not scrollable. All content and layout must dynamically fit as sized by the user.</p>
+                    <h2>不可滚动小部件</h2>
+                    <p>信息小部件不可滚动，所有内容和布局都必须根据用户设置的小部件的大小动态匹配。</p>
                   </div>
                 </section>
                 <section class="module col-1">
                   <div class="module">
-                    <h2>Responsive resizing</h2>
-                    <p>Widgets should accommodate different spacing requirements across devices, including cell number, size, and spacing variations.</p>
+                    <h2>响应式小部件</h2>
+                    <p>小部件应该适应不同设备的空间要求，包括单元格数量，大小和空间变化。</p>
                   </div>
                 </section>
               </div>
-              <h1>Configuring widgets</h1>
-              <p>Android widgets display their configuration choices once placed on a home panel. </p>
-              <p>Configuration best practices:</p>
+              <h1>配置小部件</h1>
+              <p>Android 小部件放置在主面板上时会显示其配置项。</p>
+              <p>配置的最佳实践：</p>
               <ul class="lst-kix_50zxfsiuzaou-0 start">
-                <li>Keep configuration light and don't present more than 2-3 configuration elements</li>
-                <li>Present configuration choices using dialogs, rather than full-screen, to retain the user's context
-                </li>
+                <li>保持配置的轻量，不要超过 2-3 个配置元素</li>
+                <li>当前的配置选择使用对话框，不要用全屏，以保留用户的上下文。</li>
               </ul>
-              <p>Once setup, widgets do not typically show a "Setup" or "Configuration" button.</p>
+              <p>一旦设置完，小部件通常不会再显示“设置”或“配置”按钮。</p>
             </div>
           </div>
         </div>
