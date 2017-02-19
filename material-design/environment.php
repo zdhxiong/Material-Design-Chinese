7ec2e6b2824a70467f8f35e3ffa610e9131473fa
zdhxiong <zdhxiong@gmail.com>
2017-02-19 20:02:05 +0800

    update

--- material-design/environment.php
+++ material-design/environment.php
@@ -9,19 +9,19 @@
 
       <div class="article-content chapter-intro">
         <div>
-          <h1>Material design is a three-dimensional environment containing light, material, and cast shadows.</h1>
+          <h1>Material design 是一个包含光线、材料和投射阴影的三维环境。</h1>
           <div class="col-list">
             <section class="module-module-figure col-3">
               <div class="module">
-                <p>All material objects have x, y, and z dimensions.</p>
-                <p>All material objects have a single z-axis position.</p>
-                <p>Key lights create directional shadows, and ambient light creates soft shadows.</p>
+                <p>所有的材料对象都包含 x、y、z 三个维度。</p>
+                <p>所有的材料对象都有一个 Z 轴厚度。</p>
+                <p>主光源投射出一个定向的阴影，而环境光从各个角度投射出连贯又柔和的阴影。</p>
               </div>
               <div class="module">
-                <h3><strong>Material thickness</strong></h3>
-                <p>1dp </p>
-                <h3><strong>Shadows</strong></h3>
-                <p>Shadows are created by the elevation difference between overlapping material.</p>
+                <h3><strong>材料的厚度</strong></h3>
+                <p>1dp</p>
+                <h3><strong>阴影</strong></h3>
+                <p>阴影是不同高度的材料相互叠加所产生的。</p>
               </div>
               <div class="figure">
                 <figure class="s-tag-media">
@@ -36,13 +36,13 @@
       </div>
 
       <nav class="chapter-toc">
-        <h1>Contents</h1>
+        <h1>内容</h1>
         <ul>
           <li>
-            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="environment.html#environment-3d-world" data-qp-ui-data-id="qp:1">3D world</a>
+            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="environment.html#environment-3d-world" data-qp-ui-data-id="qp:1">3D 世界</a>
           </li>
           <li>
-            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="environment.html#environment-light-shadow" data-qp-ui-data-id="qp:2">Light and shadow</a>
+            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="environment.html#environment-light-shadow" data-qp-ui-data-id="qp:2">光线和阴影</a>
           </li>
         </ul>
       </nav>
@@ -51,7 +51,7 @@
 
         <div id="environment-3d-world" class="article qp-ui" data-qp-ui="{ 'ScrollSpy': {} }" data-qp-ui-data-id="qp:1">
           <h2 class="article-title qp-ui" data-qp-ui="{ 'ResponsiveZippy': {} }" data-qp-ui-data-id="qp:1">
-            3D world
+            3D 世界
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="icon icon-expand-collapse" role="img" viewBox="0 0 10 10" aria-labelledby="title desc">
               <title>Expand and collapse content</title>
               <desc>An arrow that points down when collapsed and points up when expanded.</desc>
@@ -62,8 +62,8 @@
             <div>
               <div class="col-list">
                 <section class="module-figure col-2">
-                  <div class="module"><p>The material environment is a 3D space, which means all objects have x, y, and z dimensions. The z-axis is perpendicularly aligned to the plane of the display, with the positive z-axis extending towards the viewer. Every sheet of material occupies a single position along the z-axis and has a standard 1dp thickness, equivalent to <a href="../layout/units-measurements.html#units-measurements-density-independent-pixels-dp-">one  pixel of thickness</a> on screens with a pixel density of 160.</p>
-                    <p>On the web, the z-axis is used for layering and not for perspective. The 3D world is emulated by manipulating the y-axis.</p>
+                  <div class="module"><p>材料所处的环境是一个 3D 空间，这意味着每个对象都有 x、y、z 三维坐标属性，z 轴垂直于显示平面，并延伸向用户视角，每个材料都有 z 轴厚度，标准是 1dp，相当于一个屏幕密度为 160 的设备上的<a href="../layout/units-measurements.html#units-measurements-density-independent-pixels-dp-">一像素</a>。</p>
+                    <p>在网页上，z 轴被用来分层而不是为了视角。3D 空间通过操纵 y 轴进行仿真。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -71,7 +71,7 @@
                         <img alt="" src="../static/media/material-design/environment/whatismaterial_environment_3d.png"/>
                       </div>
                       <figcaption>
-                        <p>3D space with x, y, and z axes</p>
+                        <p>具有 x、y、z 轴的 3D 空间</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -83,7 +83,7 @@
 
         <div id="environment-light-shadow" class="article qp-ui" data-qp-ui="{ 'ScrollSpy': {} }" data-qp-ui-data-id="qp:2">
           <h2 class="article-title qp-ui" data-qp-ui="{ 'ResponsiveZippy': {} }" data-qp-ui-data-id="qp:2">
-            Light and shadow
+            光线和阴影
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="icon icon-expand-collapse" role="img" viewBox="0 0 10 10" aria-labelledby="title desc">
               <title>Expand and collapse content</title>
               <desc>An arrow that points down when collapsed and points up when expanded.</desc>
@@ -92,8 +92,8 @@
           </h2>
           <div class="article-content">
             <div>
-              <p>Within the material environment, virtual lights illuminate the scene. Key lights create directional shadows, while ambient light creates soft shadows from all angles.</p>
-              <p>Shadows in the material environment are cast by these two light sources. In Android development, shadows occur when light sources are blocked by sheets of material at various positions along the z-axis. On the web, shadows are depicted by manipulating the y-axis only. The following example shows the card with a height of 6dp.</p>
+              <p>在材料环境中，虚拟的光线照射使场景中的对象投射出阴影，直射光投射出一个定向的阴影，而环境光从各个角度投射出连贯又柔和的阴影。</p>
+              <p>材料环境中的所有阴影都是由这两种光投射产生的，阴影是光线照射不到的地方，因为各个元素在 z 轴上占据了不同大小的位置，遮住了这些光线。在网页上，阴影的实现是在 y 轴上使用多重阴影。下面的案例中，卡片的高度是 6dp。</p>
               <div class="col-list">
                 <section class="figure-figure-figure col-3">
                   <div class="figure">
@@ -102,7 +102,7 @@
                         <img alt="" src="../static/media/material-design/environment/whatismaterial_environment_shadow1.png"/>
                       </div>
                       <figcaption>
-                        <p>Shadow cast by key light</p>
+                        <p>直射光投射的阴影</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -112,7 +112,7 @@
                         <img alt="" src="../static/media/material-design/environment/whatismaterial_environment_shadow2.png"/>
                       </div>
                       <figcaption>
-                        <p>Shadow cast by ambient light</p>
+                        <p>环境光投射的阴影</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -122,7 +122,7 @@
                         <img alt="" src="../static/media/material-design/environment/whatismaterial_environment_shadow3.png"/>
                       </div>
                       <figcaption>
-                        <p>Combined shadow from key and ambient lights</p>
+                        <p>直射光和环境光混合投影</p>
                       </figcaption>
                     </figure>
                   </div>
