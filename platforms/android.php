7c9a123f6ac2bc302a936bb05b3f0ac407088d0e
zdhxiong <zdhxiong@gmail.com>
2017-02-20 11:51:17 +0800

    update

--- platforms/android.php
+++ platforms/android.php
@@ -9,13 +9,13 @@
 
       <div class="article-content chapter-intro">
         <div>
-          <h1>Material Design has Android-only guidance for several interactions.</h1>
-          <p>The following guidance refers specifically to these interactions on Android:</p>
+          <h1>Material Design 的多种交互方式是 Android 平台特有的，我们专门提供了这些交互的设计指南。</h1>
+          <p>以下设计指南专门针对 Android 上的交互效果：</p>
           <ul class="lst-kix_8k696vbjtkz9-0 start">
-            <li><a href="../patterns/notifications.html">Notifications</a></li>
+            <li><a href="../patterns/notifications.html">通知</a></li>
             <li><a href="../components/snackbars-toasts.html">Snackbars and toasts</a></li>
-            <li><a href="../patterns/fingerprint.html">Fingerprint</a></li>
-            <li><a href="../layout/split-screen.html">Split screen</a></li>
+            <li><a href="../patterns/fingerprint.html">指纹</a></li>
+            <li><a href="../layout/split-screen.html">分屏</a></li>
           </ul>
         </div>
       </div>
