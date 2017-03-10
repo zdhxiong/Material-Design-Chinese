ec3fa27e4d582bfae0c46478abb3cb0f140238c7
zdhxiong <zdhxiong@gmail.com>
2017-03-10 17:28:42 +0800

    update

--- patterns/loading-images.php
+++ patterns/loading-images.php
@@ -9,19 +9,19 @@
 
       <div class="article-content chapter-intro">
         <div>
-          <h1>Images, including illustrations and photographs, may load in three phases.</h1>
+          <h1>图片，包括插图和照片，可分三个阶段加载。</h1>
           <div class="col-list">
             <section class="module-module-figure col-3">
               <div class="module">
-                <p>Illustrations and photographs may load and transition in three phases by staggering opacity, exposure, and saturation levels.</p>
+                <p>插图和照片会通过不透明度、曝光和饱和度三个错开的阶段来加载和过渡。</p>
               </div>
               <div class="module">
-                <h3><strong>Behavior</strong></h3>
-                <p>Progressive fade-in<br>
-                  Duration<br>
-                  Animation</p>
-                <h3><strong>Duration </strong></h3>
-                <p>Use longer durations for loading images, and shorter durations for transitions.</p>
+                <h3><strong>行为</strong></h3>
+                <p>渐进式淡入<br>
+                  转换时长<br>
+                  动画</p>
+                <h3><strong>转换时长</strong></h3>
+                <p>使用较长的时间来载入图片，并缩短过渡效果的持续时间。</p>
               </div>
               <div class="figure">
                 <figure class="s-tag-media">
@@ -39,10 +39,10 @@
         <h1>目录</h1>
         <ul>
           <li>
-            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="loading-images.html#loading-images-usage" data-qp-ui-data-id="qp:1">Usage</a>
+            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="loading-images.html#loading-images-usage" data-qp-ui-data-id="qp:1">用法</a>
           </li>
           <li>
-            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="loading-images.html#loading-images-behavior" data-qp-ui-data-id="qp:2">Behavior</a>
+            <a class="gweb-smoothscroll-control qp-ui" data-qp-ui="{ 'SmoothScroll': {} }" href="loading-images.html#loading-images-behavior" data-qp-ui-data-id="qp:2">行为</a>
           </li>
         </ul>
       </nav>
@@ -51,7 +51,7 @@
 
         <div id="loading-images-usage" class="article qp-ui" data-qp-ui="{ 'ScrollSpy': {} }" data-qp-ui-data-id="qp:1">
           <h2 class="article-title qp-ui" data-qp-ui="{ 'ResponsiveZippy': {} }" data-qp-ui-data-id="qp:1">
-            Usage
+            用法
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="icon icon-expand-collapse" role="img" viewBox="0 0 10 10" aria-labelledby="title desc">
               <title>Expand and collapse content</title>
               <desc>An arrow that points down when collapsed and points up when expanded.</desc>
@@ -63,9 +63,9 @@
               <div class="col-list">
                 <section class="module-figure col-2">
                   <div class="module">
-                    <p>Illustrations and photographs may load and transition in three phases at staggered durations, rather than relying on opacity changes alone. You may combine image opacity, exposure, and saturation changes to suit your needs.</p>
-                    <h2>Exposure adjustments</h2>
-                    <p>Images should begin loading with low contrast levels and desaturated color. Once image opacity reaches 100%, display the image with full-color saturation. </p>
+                    <p>插图和照片可以在错开的时长内在三个阶段之间加载和过渡，而不是单独依赖于不透明度的变化。你可以结合图片不透明度、曝光和饱和度变化来满足你的需求。</p>
+                    <h2>曝光调整</h2>
+                    <p>图片应该从低对比度和低饱和度开始加载。一旦图片的不透明度达到 100%，就以完整的饱和度显示图片。</p>
                   </div>
                   <div class="figure">
                     <figure class="s-tag-media">
@@ -73,10 +73,10 @@
                         <img alt="" src="<?php static_url('../static/media/patterns/loading-images/patterns_loadingimages1.png'); ?>"/>
                       </div>
                       <figcaption>
-                        <p>Opacity, exposure, and saturation recommendations for loading images</p>
-                        <p>Opacity: 0% start, 100% end<br>
-                          Exposure: Gamma, Black output (for darker images)<br>
-                          Saturation: 0 or 20% start, 100% end</p>
+                        <p>加载图片时推荐使用的不透明度、曝光和饱和度</p>
+                        <p>不透明度：0% 开始，100% 结束<br>
+                          曝光：Gamma, Black output（针对暗色图片）<br>
+                          饱和度：0 或 20% 开始，100% 结束</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -91,7 +91,7 @@
                         <img alt="" src="<?php static_url('../static/media/patterns/loading-images/patterns-imagetreatment-imageload3_large_xhdpi.png'); ?>"/>
                       </div>
                       <figcaption>
-                        <p>Full opacity and exposure</p>
+                        <p>完全不透明和曝光</p>
                       </figcaption>
                     </figure>
                   </div>
@@ -101,7 +101,7 @@
                         <img alt="" src="<?php static_url('../static/media/patterns/loading-images/patterns-imagetreatment-imageload4_large_xhdpi.png'); ?>"/>
                       </div>
                       <figcaption>
-                        <p>Color saturation</p>
+                        <p>色彩饱和度</p>
                       </figcaption>
                     </figure>
                   </div>
