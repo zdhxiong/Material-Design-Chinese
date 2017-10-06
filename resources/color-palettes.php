24eb7e96f83650d8920180d07434860fb8d5560f
zdhxiong <zdhxiong@gmail.com>
2017-10-06 15:59:59 +0800

    2017.09 - 资源 - 颜色表

--- resources/color-palettes.php
+++ resources/color-palettes.php
@@ -12,18 +12,16 @@
         <div>
           <p>这个 zip 文件包含 Adobe Photoshop 和 Adobe Illustrator 的颜色色板，你可以直接下载安装。zip 文件中的 Read-Me.pdf 是安装指南，它会教你如何把 .aco 文件安装到 Photoshop，以及如何把 .ase 文件安装到 Illustrator。</p>
           <p>关于 Material Design 中的更多关于颜色的信息，详见<a href="../style/color.html">颜色</a>。</p>
-          <p>
-            <div class="modulettes">
-              <a class="modulette download trackdl" download="" href="<?php static_url('../static/media/resources/color-palettes/color_swatches.zip'); ?>" title="Color Swatches">
-                <img alt="" class="icon download-icon" src="<?php static_url('../static/spec/images/callouts/download.svg'); ?>">
-                <div class="info">
-                  <div class="title">颜色色板</div>
-                  <span class="subtitle">0.02 MB (.zip) </span>
-                </div>
-                </img>
-              </a>
-            </div>
-          </p>
+          <div class="modulettes">
+            <a class="modulette download trackdl" download="" href="<?php static_url('../static/media/resources/color-palettes/color_swatches.zip'); ?>" title="Color Swatches">
+              <img alt="" class="icon download-icon" src="<?php static_url('../static/spec/images/callouts/download.svg'); ?>">
+              <div class="info">
+                <div class="title">颜色色板</div>
+                <span class="subtitle">0.02 MB (.zip) </span>
+              </div>
+              </img>
+            </a>
+          </div>
         </div>
       </div>
     </div>
